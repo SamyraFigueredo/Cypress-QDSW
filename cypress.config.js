bc0bab99133fde04a87a1dc2c51807a3284edd90
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-11-25 20:47:00 -0300

    refatorando configuracoes

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    projectId: "yg22sw",
-    
     e2e: {
       setupNodeEvents(on, config) {
       // implement node event listeners here
