6e96b94a95d0dea8769b2eeacd979e9420fc5188
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-11-30 22:06:20 -0300

    adiciona teste de acesso à página inicial da Petstore.swagger

--- cypress/e2e/petstore.swagger.cy.js
+++ cypress/e2e/petstore.swagger.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Pagina inicial so site', () => {
+  it('Acessa o site', () => {
     cy.visit('https://petstore.swagger.io/')
+
+    // Vai verificar se esta no domínio correto
+    cy.url().should('include', 'petstore.swagger.io')
   })
 })
